6b84aac6ab82c9a8301141f5861bb869cb069e80
Aaryan Jain <aaryan@Aaryans-Laptop.local>
2024-08-28 21:52:48 +0530

    add env

--- chatting-with-selected-people-backend/backend/settings.py
+++ chatting-with-selected-people-backend/backend/settings.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
+from dotenv import load_dotenv
 from pathlib import Path
-
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v)1p9=)68#819u0z6*!=zzr$$-bw=b*9mwd_qk@!1-*f45f(%^'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -96,9 +97,9 @@ CHANNEL_LAYERS = {
 DATABASES ={
     'default' : {
         "ENGINE" : "django.db.backends.postgresql",
-        "NAME" : "chatting",
-        "USER": "chatting",
-        "PASSWORD":"password",
+        "NAME" : os.getenv('database_name'),
+        "USER": os.getenv('database_user'),
+        "PASSWORD":os.getenv('database_password'),
         "HOST":"127.0.0.1",
         "PORT":"5432",
     }
